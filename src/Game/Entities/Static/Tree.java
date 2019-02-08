5588f81238c868117574f514026d7b89dd9fe22f
raldy <raldy@DESKTOP-9QO0AN7>
2019-02-08 01:45:09 -0400

    Completed the Tree Code in GameEntities(Tree.java)

--- src/Game/Entities/Static/Tree.java
+++ src/Game/Entities/Static/Tree.java
@@ -4,21 +4,30 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 import Main.Handler;
+import Resources.Images;
 
 public class Tree extends StaticBase {
-
-	public Tree(Handler handler) {
+	
+	private Rectangle tree;
+		
+	public Tree(Handler handler, int xPosition, int yPosition) {
 		super(handler);
+		this.setY(yPosition);
+        this.setX(xPosition);
+        
 	}
 
 	@Override
 	public void render(Graphics g) {
 
+		g.drawImage(Images.bigtree, this.getX(), this.getY(), 64, 64, null);
+    	tree = new Rectangle(this.getX(), this.getY()+5, 64, 55);
+		
 	}
 	
     @Override
     public Rectangle GetCollision() {
     	
-    	return new Rectangle();
+    	return tree;
     }
 }
