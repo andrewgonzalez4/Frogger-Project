bee12a5d915958c2a8e2f467d55dac592c27b0e8
raldy <raldy@DESKTOP-9QO0AN7>
2019-02-24 22:36:07 -0400

    Added code to get a message in the lower left corner of the screen.

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -2,6 +2,8 @@ package Display;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import javax.imageio.ImageIO;
@@ -15,23 +17,44 @@ public class DisplayScreen {
 
     private JFrame frame;
     private Canvas canvas;
+    
 
     private URL iconURL;
 
     private String title;
     private int width, height;
+    private Font messageFont;
+    private static JLabel message;
 
     public DisplayScreen(String title, int width, int height){
         this.title = title;
         this.width = width;
         this.height = height;
 
-
-
         createDisplay();
     }
 
-    private void createDisplay(){
+    public void setBackgroundColor(Color backgroundColor) {
+		canvas.setBackground(backgroundColor);
+    }
+    
+    private void createDisplay() {
+    	try {
+			messageFont = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("res/Fonts/aerial.ttf"))).deriveFont(Font.PLAIN, 14);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (FontFormatException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	message = new JLabel("It's Frogger Time!");
+    	message.setFont(messageFont);
+    	
+    	
         frame = new JFrame(title);
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -39,6 +62,7 @@ public class DisplayScreen {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setBackground(Color.black);
+        frame.add(message, BorderLayout.SOUTH);
 
         try {
             frame.setIconImage(ImageIO.read(new File("res/Sheets/froggy.png")));
@@ -65,4 +89,13 @@ public class DisplayScreen {
         return frame;
     }
 
+    public static void setMessage(String text) {
+		message.setText(text);
+	}
+    public static void enableMessage() {
+    	message.setVisible(true);
+    }
+    public static void disableMessage() {
+    	message.setVisible(false);;
+    }
 }
